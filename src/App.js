b73ece369ac5b95ff847d7c6c7ccdd45f414f9f5
hlee2542 <ou40a9rtbp@gmail.com>
2020-01-22 14:17:42 -0800

    added image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      alt: 'Loading.',
+      text: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => res.json())
+        .then(data => {
+          this.setState({
+            url: data.hdurl,
+            alt: data.explanation,
+            text: data.explanation
+          });
+        })
+        .catch(err => this.setState({alt: 'Image Failed to Load.'}));
+  }
+
+  render() {
+    return <div className='App'>
+      <h1>NASA Astronomy Photo of the Day</h1>
+      <div className='photo-container'>
+        <img src={this.state.url} alt={this.state.alt} />
+      </div>
+      <p>{this.state.text}</p>
     </div>
-  );
+  }
 }
 
 export default App;
