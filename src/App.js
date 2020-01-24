c55aafefb147eb447ce84897d10a129e504359d1
hlee2542 <ou40a9rtbp@gmail.com>
2020-01-23 23:08:33 -0800

    made app functional instead of class-based

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: '',
-      alt: 'Loading.',
-      text: ''
-    }
-  }
-
-  componentDidMount() {
+const App = () => {
+  let [hdurl, setUrl] = useState('');
+  let [alt, setAlt] = useState('Loading.');
+  let [text, setText] = useState('');
+  useEffect(() => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(res => res.json())
         .then(data => {
-          this.setState({
-            url: data.hdurl,
-            alt: data.explanation,
-            text: data.explanation
-          });
+          setUrl(data.hdurl);
+          setAlt(data.explanation);
+          setText(data.explanation);
         })
-        .catch(err => this.setState({alt: 'Image Failed to Load.'}));
-  }
-
-  render() {
-    return <div className='App'>
-      <h1>NASA Astronomy Photo of the Day</h1>
-      <div className='photo-container'>
-        <img src={this.state.url} alt={this.state.alt} />
-      </div>
-      <p>{this.state.text}</p>
+        .catch(err => setAlt('Image Failed to Load.'));
+  }, []);
+  return <div className='App'>
+    <h1>NASA Astronomy Photo of the Day</h1>
+    <div className='photo-container'>
+      <img src={hdurl} alt={alt} />
     </div>
-  }
+    <p>{text}</p>
+  </div>
 }
 
 export default App;
