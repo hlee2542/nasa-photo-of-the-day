aacd2f066c7228568fdb246ee14e9f792b478ee5
hlee2542 <ou40a9rtbp@gmail.com>
2020-01-23 23:58:56 -0800

    made style changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
+import styled from 'styled-components';
+import { Card, Button, CardTitle, CardText, CardImg, Row, Col, Container } from 'reactstrap';
+
+const CenterImg = styled.img`
+  display: block;
+  margin: 0% auto;
+`;
+
+const CenterTitle = styled.h1`
+  text-align: center;
+`;
 
 const App = () => {
-  let [hdurl, setUrl] = useState('');
+  let [hdurl, setHDUrl] = useState('');
+  let [url, setUrl] = useState('');
   let [alt, setAlt] = useState('Loading.');
   let [text, setText] = useState('');
   useEffect(() => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(res => res.json())
         .then(data => {
-          setUrl(data.hdurl);
+          setHDUrl(data.hdurl);
+          setUrl(data.url);
           setAlt(data.explanation);
           setText(data.explanation);
         })
         .catch(err => setAlt('Image Failed to Load.'));
   }, []);
-  return <div className='App'>
-    <h1>NASA Astronomy Photo of the Day</h1>
-    <div className='photo-container'>
-      <img src={hdurl} alt={alt} />
-    </div>
-    <p>{text}</p>
-  </div>
+  return <Container>
+      <Row>
+        <Col>
+          <Card body>
+            <CenterTitle>NASA Astronomy Photo of the Day</CenterTitle>
+            <a href={hdurl}><CenterImg src={url} alt={alt} /></a>
+            <CardText>{text}</CardText>
+          </Card>
+        </Col>
+      </Row>
+  </Container>
 }
 
 export default App;
